ccf660ada634075388a3617e2c2f9b469c7dc788
farobledo <fabian.robledolave@gmail.com>
2022-10-24 19:21:12 -0500

    crud_plataforma

---
+++ vistas/home.php
@@ -0,0 +1,9 @@
+<div class="container is-fluid">
+	<h1 class="title">OPTIVISIÓN</h1>
+	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+   
+  </div>
+  <div class="card-footer text-muted">
+    24/10/2022
+  </div>
+</div>
